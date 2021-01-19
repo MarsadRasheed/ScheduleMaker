748d756c8a952bbcc155c532a2ad33fc0b125832
Marsad Rasheed <54879111+MarsadRasheed@users.noreply.github.com>
2021-01-20 01:53:42 +0500

    Add files via upload

---
+++ source/DayArrayList.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class DayArrayList {
+
+    private ArrayList<Day> days;
+
+    public DayArrayList(ArrayList<Day> days) {
+        this.days = days;
+    }
+
+    public ArrayList<Day> getDays() {
+        return days;
+    }
+
+    public void setDays(ArrayList<Day> days) {
+        this.days = days;
+    }
+
+}
